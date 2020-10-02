8081984c08c53e191d9eb372a62adb29f2d94627
Marjan Mukram <marjanmukram8@gmail.com>
2020-10-02 16:02:11 +0530

    mock a component

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders save to reload paragraph', () => {
-  const { getByText } = render(<App />);
-  const  paragraphText = getByText(/save to reload/i);
-  expect(paragraphText).toBeInTheDocument();
-});
+// test('renders save to reload paragraph', () => {
+//   const { getByText } = render(<App />);
+//   const  paragraphText = getByText(/save to reload/i);
+//   expect(paragraphText).toBeInTheDocument();
+// });
+
+jest.mock('./CheckboxWithLabel', () => () => <div>Hello World</div>);
+
+test('renders', () => {
+  const {container} = render(<App />);
+  expect(container.textContent).toMatch('save')
+})
