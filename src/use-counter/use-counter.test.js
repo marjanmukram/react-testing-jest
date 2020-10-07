f15d15a0730294b79c960f62362d01265a00fe7e
Marjan Mukram <marjanmukram8@gmail.com>
2020-10-07 23:34:52 +0530

    more use counter tests

--- src/use-counter/use-counter.test.js
+++ src/use-counter/use-counter.test.js
@@ -11,10 +11,41 @@ test("exposes count and increment/decrement functions", () => {
   };
   render(<TestComponent />);
   expectExport(result.count).toBe(0);
-  
+
   act(() => result.increment());
   expectExport(result.count).toBe(1);
-  
+
   act(() => result.decrement());
   expectExport(result.count).toBe(0);
 });
+
+test("allows customization of initial count value", () => {
+  let result;
+  const TestComponent = () => {
+    result = useCounter({ initialCount: 2 });
+    return null;
+  };
+  render(<TestComponent />);
+  expectExport(result.count).toBe(2);
+  act(() => result.increment());
+  expectExport(result.count).toBe(3);
+
+  act(() => result.decrement());
+  expectExport(result.count).toBe(2);
+});
+
+test("allows customization of step value", () => {
+    let result;
+    const TestComponent = () => {
+      result = useCounter({step:2});
+      return null;
+    };
+    render(<TestComponent />);
+    expectExport(result.count).toBe(0);
+  
+    act(() => result.increment());
+    expectExport(result.count).toBe(2);
+  
+    act(() => result.decrement());
+    expectExport(result.count).toBe(0);
+  });
