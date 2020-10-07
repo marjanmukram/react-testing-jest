788c0b9b6f2d3c45086f2fe03a276e5639b915b9
Marjan Mukram <marjanmukram8@gmail.com>
2020-10-07 08:56:02 +0530

    test-data-bot

--- src/form-compoent/form-component.test.js
+++ src/form-compoent/form-component.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, fireEvent, waitFor } from "@testing-library/react";
+import { build, fake, sequence } from "test-data-bot";
 import { Redirect as MockRedirect } from "react-router";
 import { savePost as mockSavePost } from "./api";
 import FormComponent from "./form-component";
@@ -12,22 +13,27 @@ jest.mock("react-router", () => {
 afterEach(() => {
   jest.clearAllMocks();
 });
+
+const postBuilder = build("Post").fields({
+  title: fake(f => f.lorem.words()),
+  content: fake(f => f.lorem.paragraphs().replace(/\r/g,'')),
+  date: expect.any(String),
+  tags: fake(f => [f.lorem.words(), f.lorem.words(), f.lorem.words()])
+});
+
+const userBuilder = build("User").fields({
+  id: sequence(s => `user-${s}`)
+});
+
 test("FormComponent renders title,content, form button", async () => {
-  const fakeUser = {
-    id: "user-1"
-  };
+  const fakeUser = userBuilder();
   const preDate = new Date().getTime();
   const { getByLabelText, getByText } = render(
     <FormComponent user={fakeUser} />
   );
   mockSavePost.mockResolvedValueOnce();
 
-  const fakePost = {
-    title: "Test title",
-    content: "Test content",
-    date: expect.any(String),
-    tags: ["tag1", "tag2"]
-  };
+  const fakePost = postBuilder();
 
   getByLabelText(/title/i).value = fakePost.title;
   getByLabelText(/content/i).value = fakePost.content;
@@ -36,11 +42,12 @@ test("FormComponent renders title,content, form button", async () => {
   expect(submitButton).not.toBeDisabled();
   fireEvent.click(submitButton);
   expect(submitButton).toBeDisabled();
-  expect(mockSavePost).toBeCalledWith({ ...fakePost, authorId: fakeUser.id });
-  expect(mockSavePost).toHaveBeenCalledTimes(1);
+ 
   await waitFor(() =>
     expect(MockRedirect).toHaveBeenCalledWith({ to: "/" }, {})
   );
+  expect(mockSavePost).toBeCalledWith({ ...fakePost, authorId: fakeUser.id });
+  expect(mockSavePost).toHaveBeenCalledTimes(1);
   const postDate = new Date().getTime();
   const date = new Date(mockSavePost.mock.calls[0][0].date).getTime();
   expect(date).toBeGreaterThanOrEqual(preDate);
